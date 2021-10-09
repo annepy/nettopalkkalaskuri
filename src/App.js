d87d9145f2174d8573bb79f923fb23f358d546bd
Anne Pyykkönen <n0pyan00@students.oamk.fi>
2021-10-09 10:53:19 +0300

    nettopalkkalaskuri

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import {useState} from "react";
+import FillOptions from './FillOptions';
 
-function App() {
+function App () {
+  const [brutto, setBrutto] = useState("");
+  const [vero, setVero] = useState("");
+  const [veroSumma, setVeroSumma] = useState(0);
+  const [tv,setTv] = useState(1);
+  const [tyottomyysvakuutusSumma, setTyottomyysvakuutusSumma] = useState(0);
+  const [te,setTe] = useState(1);
+  const [tyoelakeSumma, setTyoelakeSumma] = useState(0);
+  const [netto,setNetto] = useState(0);
+
+  function calculate (e) {
+    e.preventDefault();
+    const veroEurot = brutto / 100 * vero;
+    const teEurot = brutto / 100 * te;
+    const tvEurot = brutto / 100 * tv;
+    setVeroSumma(veroEurot);
+    setTyoelakeSumma(tvEurot);
+    setTyottomyysvakuutusSumma(teEurot);
+    setNetto(brutto - veroEurot - teEurot - tvEurot);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+      <div id="container">
+        <h3>Palkkalaskuri</h3>
+        <form onSubmit={calculate}>
+        <div>
+          <label>Bruttopalkka</label>
+        </div>
+        <input tyype='number' value={brutto} onChange={e => setBrutto(e.target.value)}/>
+
+        <div>
+          <label>Vero (%)</label>
+          <output>{veroSumma} €</output>
+
+        </div>
+        <input tyype='number' value={vero} onChange={e => setVero(e.target.value)}/>
+
+        <div>
+          <label>Työttömyysvakuutus (%)</label>
+          <output>{tyottomyysvakuutusSumma} €</output>
+        </div>
+        <select value={tv} onChange={e => setTv(e.target.value)}>
+          <FillOptions />
+        </select>
+
+        <div>
+          <label>Työeläke (%)</label>
+          <output>{tyoelakeSumma} €</output>
+        </div>
+        <select value={te} onChange={e => setTe(e.target.value)}>
+        <FillOptions />
+        </select>
+
+        <div>
+          <label>Palkkasi</label>
+          <output id="netto">{netto}</output>
+        </div>
+
+        <button>Laske</button>
+
+        </form>
+      </div>
+    
+  )
 }
 
-export default App;
+export default App
